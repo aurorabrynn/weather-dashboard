c129d407b5366ec2251ccd9647b00b2395f51420
Aurora Duncan <aurorabrynn@gmail.com>
2021-04-04 14:44:06 -0700

    added forcast div, started styling

--- assets/script.js
+++ assets/script.js
@@ -4,19 +4,19 @@ var currentDiv = document.querySelector("#current");
 var futureDiv = document.querySelector("#future");
 var queryArr = location.search.split("&");
 var searchTerm = queryArr[0].split("=")[1];
+var i = 0;
 
 searchForm.addEventListener("submit", function (event) {
     event.preventDefault();
     var cityNameInput = cityName.value;
     var urlToFetchCurrent = `http://api.openweathermap.org/data/2.5/weather?q=${cityNameInput}&units=imperial&appid=745149043888fbd1a0283fd8a132ecf0`
-    var urlToFetchFuture = `http://api.openweathermap.org/data/2.5/forecast?q=${cityNameInput}&appid=745149043888fbd1a0283fd8a132ecf0`
+    var urlToFetchFuture = `http://api.openweathermap.org/data/2.5/forecast?q=${cityNameInput}&units=imperial&appid=745149043888fbd1a0283fd8a132ecf0`
     fetch(urlToFetchCurrent).then(function (response) {
         return response.json()
     }).then(function (data) {
         currentDiv.innerHTML = "";
         futureDiv.innerHTML = "";
         console.log(data);
-        //for (let i = 0; i < data.results.length; i++) {
         var resultBlock = document.createElement("div");
         resultBlock.setAttribute("class", "result-block");
 
@@ -54,5 +54,26 @@ searchForm.addEventListener("submit", function (event) {
         return response.json()
     }).then(function (data) {
         console.log(data)
+        for (let i = 5; i < data.list.length; i = i + 8) {
+            var resultBlock = document.createElement("div");
+            resultBlock.setAttribute("class", "result-block");
+
+            console.log(data.list[i].dt_txt);
+            var dateH3 = document.createElement("h3");
+            dateH3.textContent = data.list[i].dt_txt;
+            resultBlock.append(dateH3);
+
+            console.log(data.list[i].main.temp);
+            var tempP = document.createElement("p");
+            tempP.textContent = data.list[i].main.temp;
+            resultBlock.append(tempP);
+
+            console.log(data.list[i].main.humidity);
+            var humP = document.createElement("p");
+            humP.textContent = data.list[i].main.humidity;
+            resultBlock.append(humP);
+
+            futureDiv.append(resultBlock);
+        }
     })
 })
