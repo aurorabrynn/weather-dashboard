0d475e807067a49bee8a0f823a553132ceef3e94
Aurora Duncan <aurorabrynn@gmail.com>
2021-04-04 17:11:33 -0700

    fixed some layout

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,6 @@ var searchForm = document.querySelector("#search-form");
 var cityName = document.querySelector("#city-name");
 var currentDiv = document.querySelector("#current");
 var futureDiv = document.querySelector("#future");
-var queryArr = location.search.split("&");
-var searchTerm = queryArr[0].split("=")[1];
 var i = 0;
 
 searchForm.addEventListener("submit", function (event) {
@@ -22,22 +20,22 @@ searchForm.addEventListener("submit", function (event) {
 
         console.log(data.name);
         var titleH2 = document.createElement("h2");
-        titleH2.textContent = data.name;
+        titleH2.textContent = data.name + " (" + moment().format("M/D/YYYY") + ")";
         resultBlock.append(titleH2);
 
         console.log(data.main.temp);
         var tempP = document.createElement("p");
-        tempP.textContent = data.main.temp;
+        tempP.textContent = "Temperature: " + data.main.temp + " °F";
         resultBlock.append(tempP);
 
         console.log(data.main.humidity);
         var humP = document.createElement("p");
-        humP.textContent = data.main.humidity;
+        humP.textContent = "Humidity: " + data.main.humidity + "%";
         resultBlock.append(humP);
 
         console.log(data.wind.speed);
         var windP = document.createElement("p");
-        windP.textContent = data.wind.speed;
+        windP.textContent = "Wind Speed: " + data.wind.speed + " MPH";
         resultBlock.append(windP);
 
         /*var linkAnchor = document.createElement("a");
@@ -55,25 +53,36 @@ searchForm.addEventListener("submit", function (event) {
     }).then(function (data) {
         console.log(data)
         for (let i = 0; i < data.list.length; i = i + 8) {
-            var resultBlock = document.createElement("div");
-            resultBlock.setAttribute("class", "result-block");
+            var column = document.createElement("div");
+            var card = document.createElement("div");
+            var cardBody = document.createElement("div");
+            column.setAttribute("class", "col-lg-2");
+            card.setAttribute("class", "card");
+            cardBody.setAttribute("class", "card-body");
 
             console.log(data.list[i].dt_txt);
+            console.log(moment(data.list[i].dt_txt).format("M/D/YYYY"));
             var dateH3 = document.createElement("h3");
-            dateH3.textContent = data.list[i].dt_txt;
-            resultBlock.append(dateH3);
+            dateH3.textContent = moment(data.list[i].dt_txt).format("M/D/YYYY");
+            dateH3.setAttribute = ("class", "card-title")
+            cardBody.append(dateH3);
 
             console.log(data.list[i].main.temp);
             var tempP = document.createElement("p");
-            tempP.textContent = data.list[i].main.temp;
-            resultBlock.append(tempP);
+            tempP.textContent = "Temp: " + data.list[i].main.temp + " °F";
+            tempP.setAttribute = ("class", "card-text")
+            cardBody.append(tempP);
 
             console.log(data.list[i].main.humidity);
             var humP = document.createElement("p");
-            humP.textContent = data.list[i].main.humidity;
-            resultBlock.append(humP);
+            humP.textContent = "Humidity: " + data.list[i].main.humidity + "%";
+            humP.setAttribute = ("class", "card-text")
+            cardBody.append(humP);
 
-            futureDiv.append(resultBlock);
+            card.append(cardBody);
+            column.append(card);
+            futureDiv.append(column);
         }
+        console.log(futureDiv);
     })
 })
